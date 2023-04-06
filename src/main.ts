18aa77ccee9490e651784d6bd8979e875c37e067
alejoleo23 <101273966+alejoleo23@users.noreply.github.com>
2023-04-06 12:13:49 -0500

    init configuration main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
